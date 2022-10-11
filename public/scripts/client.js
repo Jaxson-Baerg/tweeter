b4c07d702d3119249e9c464a15e2876ee6c7fb4f
Jaxson-Baerg <jaxson.baerg@gmail.com>
2022-10-11 19:12:10 +0000

    Some styling added and implementation of jquery instead of vanilla js

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,34 +5,31 @@
  */
 
 // Keep track of tweet length and display characters left, and prevent typing after reaching maximum
-document.getElementById('tweet-text').addEventListener('keypress', (e) => {
-  tweet = document.getElementById('tweet-text');
-  tweetTracker = document.getElementById('tweet-counter');
-  tweetLength = tweet.value.length;
-  tweetLength === 140 ? tweetTracker.innerHTML = 140 - tweetLength : tweetTracker.innerHTML = 140 - tweetLength - 1;
+$tweet = $('#tweet-text');
+$tweetTracker = $('#tweet-counter');
+$tweet.on('keypress', (e) => {
+  tweetLength = $tweet.val().length;
+  tweetLength === 140 ? $tweetTracker.html(140 - tweetLength) : $tweetTracker.html(140 - tweetLength - 1);
 
   if (140 - tweetLength === 0) {
     e.preventDefault();
   }
 });
 // Check if tweet length is maximum and allow deleting to lower tweet length
-document.getElementById('tweet-text').addEventListener('keydown', (e) => {
-  tweet = document.getElementById('tweet-text');
-  tweetTracker = document.getElementById('tweet-counter');
-  tweetLength = tweet.value.length;
+$tweet.on('keydown', (e) => {
+  tweetLength = $tweet.val().length;
 
   if (e.ctrlKey && e.key === 'Backspace') {
-    tweetTracker.innerHTML = 140;
+    $tweetTracker.html(140);
   } else {
     if (e.key === 'Backspace') {
-      tweetLength === 0 ? tweetTracker.innerHTML = 140 - tweetLength : tweetTracker.innerHTML = 140 - tweetLength + 1;
+      tweetLength === 0 ? $tweetTracker.html(140 - tweetLength) : $tweetTracker.html(140 - tweetLength + 1);
     }
   }
 });
 // Check if anything pasted inside will exceed tweet length maximum, prevent if so
-document.getElementById('tweet-text').addEventListener('paste', (e) => {
-  tweet = document.getElementById('tweet-text');
-  tweetLength = tweet.value.length;
+$tweet.on('paste', (e) => {
+  tweetLength = $tweet.val().length;
 
   clipboardData = e.clipboardData || window.clipboardData;
   if (140 - (tweetLength + e.clipboardData.getData('Text').length) <= 0) {

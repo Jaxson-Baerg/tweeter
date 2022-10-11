bdb8698c2830b5dc58bdc02cae73310e46497cae
Jaxson-Baerg <jaxson.baerg@gmail.com>
2022-10-11 15:02:32 +0000

    additional styling as well as implementing counter for tweet length and fail safes around it

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -12,3 +12,36 @@
 //   console.log("Loaded");
 //   document.body.removeAttribute('hidden');
 // });
+
+// $("#tweet-text").
+window.onload = () => {
+  document.getElementById('tweet-text').addEventListener('keyup', (e) => {
+    tweet = document.getElementById('tweet-text');
+    tweetTracker = document.getElementById('tweet-counter');
+    tweetLength = tweet.value.length;
+    tweetTracker.innerHTML = 140 - tweetLength;
+  
+    if (140 - tweetLength === 0 && e.key !== 'Backspace') {
+      e.preventDefault();
+    }
+  });
+  document.getElementById('tweet-text').addEventListener('keydown', (e) => {
+    tweet = document.getElementById('tweet-text');
+    tweetTracker = document.getElementById('tweet-counter');
+    tweetLength = tweet.value.length;
+    tweetTracker.innerHTML = 140 - tweetLength;
+
+    if (e.key === 'Backspace') {
+      tweetTracker.innerHTML = 140 - tweetLength;
+    }
+  });
+  document.getElementById('tweet-text').addEventListener('paste', (e) => {
+    tweet = document.getElementById('tweet-text');
+    tweetLength = tweet.value.length;
+
+    clipboardData = e.clipboardData || window.clipboardData;
+    if (140 - (tweetLength + e.clipboardData.getData('Text').length) <= 0) {
+      e.preventDefault();
+    }
+  });
+}
